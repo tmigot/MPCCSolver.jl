45700f27dc9ac72241cf94fd3848e424711e5974
tmigot <tangi.migot@gmail.com>
2017-06-29 15:03:55 -0400

    Add files via upload

--- tests/cute_test.jl
+++ tests/cute_test.jl
@@ -9,14 +9,18 @@ using OutputRelaxationmod
 using CUTEst
 using NLPModels
 
-problems = CUTEst.select()
+problems_unconstrained = CUTEst.select(contype="unc")
+problems_linconstrained = CUTEst.select(contype="linear")
+problems_boundconstrained = CUTEst.select(contype="bounds")
+problems_quadconstrained = CUTEst.select(contype="quadratic")
+problems_genconstrained = CUTEst.select(contype="general")
 
-nlp = CUTEstModel(problems[4])
+nlp = CUTEstModel(problems_quadconstrained[1])
 print(nlp)
 
 #déclare le mpcc :
 @printf("Create an MPCC \n")
-exemple_nlp=MPCCmod.MPCC(nlp)
+@time exemple_nlp=MPCCmod.MPCC(nlp)
 
 @printf("%i %i %i %i %i %i %i %i \n",nlp.counters.neval_obj,
         nlp.counters.neval_cons,nlp.counters.neval_grad,
@@ -26,11 +30,7 @@ exemple_nlp=MPCCmod.MPCC(nlp)
 
 @printf("Butterfly method:\n")
 #résolution avec ALAS Butterfly
-xb,fb,orb = MPCCsolve.solve(exemple_nlp)
-@printf("%i %i %i %i %i %i %i %i \n",nlp.counters.neval_obj,
-        nlp.counters.neval_cons,nlp.counters.neval_grad,
-        nlp.counters.neval_hess,exemple_nlp.G.counters.neval_cons,
-        exemple_nlp.G.counters.neval_jac,exemple_nlp.H.counters.neval_cons
-        ,exemple_nlp.H.counters.neval_jac)
+@time xb,fb,orb,nb_eval = MPCCsolve.solve(exemple_nlp)
+@show nb_eval
 
 finalize(nlp)
--- tests/test_article.jl
+++ tests/test_article.jl
@@ -9,20 +9,12 @@ using OutputRelaxationmod
 using PyPlot
 
 using NLPModels
+using JuMP
 
 #Est-ce que l'on veut les figures :
 plot=false
 
-# Exemple 1 :
- nb_comp=1
- f(x)=x[1]-x[2]
- Gfunc(x)=x[1]
- Hfunc(x)=x[2]
- c(x)=[1-x[2]]
- lcon=[0.0]
- ucon=[Inf]
- G=SimpleNLPModel(f, collect([1.0,1.0]), lvar=-Inf*ones(2), uvar=Inf*ones(2), c=Gfunc,  lcon=zeros(nb_comp), ucon=Inf*ones(nb_comp), J=x->[1.0 0.0], Jtp=(x,v)->[v[1], 0.0])
- H=SimpleNLPModel(f, collect([1.0,1.0]), lvar=-Inf*ones(2), uvar=Inf*ones(2), c=Hfunc,  lcon=zeros(nb_comp), ucon=Inf*ones(nb_comp), J=x->[0.0 1.0], Jtp=(x,v)->[0.0, v[1]])
+println("\n \n ALAS Exemple 1 :")
 
 r0=0.1
 s0=0.1
@@ -32,17 +24,31 @@ sig_r=0.1
 sig_s=0.1
 sig_t=0.01
 
-#déclare le mpcc :
-@time ex1= MPCCmod.MPCC(f,ones(2),G,H,1,-Inf*ones(2),Inf*ones(2),c,lcon,ucon)
+ex1=JuMP.Model()
+JuMP.@variable(ex1,x[1:2],start=1.0)
+JuMP.@NLobjective(ex1,Min,x[1]-x[2])
+JuMP.@constraint(ex1,1-x[2]>=0)
+ex1=MathProgNLPModel(ex1)
+G=JuMP.Model()
+JuMP.@variable(G,x[1:2],start=1.0)
+JuMP.@constraint(G,x[1]>=0)
+JuMP.@NLobjective(G,Min,0.0)
+G=MathProgNLPModel(G)
+H=JuMP.Model()
+JuMP.@variable(H,x[1:2],start=1.0)
+JuMP.@constraint(H,x[2]>=0)
+JuMP.@NLobjective(H,Min,0.0)
+H=MathProgNLPModel(H)
 
-println("\n \n ALAS Exemple 1 :")
-#println("KDB method:")
-#résolution avec ALAS KDB
-#xkdb,fkdb,statkdb = MPCCsolve.solve(ex1,r0,sig_r,s0,sig_s,0.0,sig_t)
+@time ex1= MPCCmod.MPCC(ex1,G,H)
+
+
 println("Butterfly method:")
 #résolution avec ALAS Butterfly
 @time xb,fb,orb = MPCCsolve.solve(ex1,r0,sig_r,s0,sig_s,t0,sig_t)
- break
+
+
+ 
 if plot
  subplot(311)
  suptitle("Quadratic penalty and Newton spectral")
@@ -57,15 +63,23 @@ if plot
 end
 
 # Exemple 2 :
- f(x)=0.5*((x[1]-1)^2+(x[2]-1)^2)
- Gfunc(x)=x[1]
- nb_comp=1
- Hfunc(x)=x[2]
- G=SimpleNLPModel(f, collect([1.0,1.0]), lvar=-Inf*ones(2), uvar=Inf*ones(2), c=Gfunc,  lcon=zeros(nb_comp), ucon=Inf*ones(nb_comp), J=x->[1.0 0.0], Jtp=(x,v)->[v[1], 0.0])
- H=SimpleNLPModel(f, collect([1.0,1.0]), lvar=-Inf*ones(2), uvar=Inf*ones(2), c=Hfunc,  lcon=zeros(nb_comp), ucon=Inf*ones(nb_comp), J=x->[0.0 1.0], Jtp=(x,v)->[0.0, v[1]])
- c(x)=zeros(1)
- lcon=zeros(1)
- ucon=ones(1)
+println("\n \n ALAS Exemple 2 :")
+ex2=JuMP.Model()
+JuMP.@variable(ex2,x[1:2],start=1.0)
+JuMP.@NLobjective(ex2,Min,0.5*((x[1]-1)^2+(x[2]-1)^2))
+ex2=MathProgNLPModel(ex2)
+G=JuMP.Model()
+JuMP.@variable(G,x[1:2],start=1.0)
+JuMP.@constraint(G,x[1]>=0)
+JuMP.@NLobjective(G,Min,0.0)
+G=MathProgNLPModel(G)
+H=JuMP.Model()
+JuMP.@variable(H,x[1:2],start=1.0)
+JuMP.@constraint(H,x[2]>=0)
+JuMP.@NLobjective(H,Min,0.0)
+H=MathProgNLPModel(H)
+
+@time ex2= MPCCmod.MPCC(ex2,G,H)
 
 r0=0.1
 s0=0.1
@@ -73,16 +87,16 @@ t0=sqrt(r0)
 sig_r=0.1
 sig_s=0.1
 sig_t=0.01
-println("\n \n ALAS Exemple 2 :")
+
 
 #déclare le mpcc :
-ex2= MPCCmod.MPCC(f,collect([1.0,1.0]),G,H,1,-Inf*ones(2),Inf*ones(2),c,lcon,ucon)
+#ex2= MPCCmod.MPCC(f,collect([1.0,1.0]),G,H,1,-Inf*ones(2),Inf*ones(2),c,lcon,ucon)
 #println("KDB method:")
 #résolution avec ALAS KDB
 #xkdb,fkdb,statkdb = MPCCsolve.solve(ex2,r0,sig_r,s0,sig_s,0.0,sig_t)
 println("Butterfly method:")
 #résolution avec ALAS Butterfly
-xb,fb,orb = MPCCsolve.solve(ex2,r0,sig_r,s0,sig_s,t0,sig_t)
+@time xb,fb,orb = MPCCsolve.solve(ex2,r0,sig_r,s0,sig_s,t0,sig_t)
 
 if plot
  subplot(312)
@@ -100,19 +114,31 @@ if plot
  PyPlot.title("Ex2 : min 0.5*((x1-1)^2+(x2-1)^2) s.t. 0<=x_1 _|_ x_2 >=0")
 end
 
+println("\n \n ALAS Exemple 3 :")
 # Exemple 3 :
 # minimize x1+x2-x3
 # s.t.     0<=x1 _|_ x2>=0
 #          -4x1+x3<=0
 #          -4x2+x3<=0
- f(x)=x[1]+x[2]-x[3]
- Gfunc(x)=[x[1]+0*x[2]+0*x[3]]
- Hfunc(x)=[x[2]+0*x[1]+0*x[3]]
- c(x)=[-4*x[1]+x[3];-4*x[2]+x[3]]
- lcon=-Inf*ones(2)
- ucon=zeros(2)
- G=SimpleNLPModel(f, collect([1.0,1.0]), lvar=-Inf*ones(2), uvar=Inf*ones(2), c=Gfunc,  lcon=zeros(nb_comp), ucon=Inf*ones(nb_comp), J=x->[1.0 0.0], Jtp=(x,v)->[v[1], 0.0])
- H=SimpleNLPModel(f, collect([1.0,1.0]), lvar=-Inf*ones(2), uvar=Inf*ones(2), c=Hfunc,  lcon=zeros(nb_comp), ucon=Inf*ones(nb_comp), J=x->[0.0 1.0], Jtp=(x,v)->[0.0,v[1]])
+
+ex3=JuMP.Model()
+JuMP.@variable(ex3,x[1:3],start=1.0)
+JuMP.@NLobjective(ex3,Min,x[1]+x[2]-x[3])
+JuMP.@NLconstraint(ex3,c1,-4*x[1]+x[3]<=0)
+JuMP.@NLconstraint(ex3,c2,-4*x[2]+x[3]<=0)
+ex3=MathProgNLPModel(ex3)
+G=JuMP.Model()
+JuMP.@variable(G,x[1:3],start=1.0)
+JuMP.@constraint(G,x[1]>=0)
+JuMP.@NLobjective(G,Min,0.0)
+G=MathProgNLPModel(G)
+H=JuMP.Model()
+JuMP.@variable(H,x[1:3],start=1.0)
+JuMP.@constraint(H,x[2]>=0)
+JuMP.@NLobjective(H,Min,0.0)
+H=MathProgNLPModel(H)
+
+@time ex3= MPCCmod.MPCC(ex3,G,H)
 
 r0=0.1
 s0=0.1
@@ -121,17 +147,15 @@ sig_r=0.1
 sig_s=0.1
 sig_t=0.01
 
-println("\n \n ALAS Exemple 3 :")
-
 #déclare le mpcc :
 #ex3= MPCCmod.MPCC(f,[0.5;1.0;0.0],G,H,1,-Inf*ones(3),Inf*ones(3),c,ones(2),lcon,ucon)
 #println("KDB method:")
 #résolution avec ALAS KDB
 #xkdb,fkdb,orkdb = MPCCsolve.solve(ex3,r0,sig_r,s0,sig_s,0.0,sig_t)
-ex3= MPCCmod.MPCC(f,[0.5;1.0;0.0],G,H,1,-Inf*ones(3),Inf*ones(3),c,lcon,ucon)
+#ex3= MPCCmod.MPCC(f,[0.5;1.0;0.0],G,H,1,-Inf*ones(3),Inf*ones(3),c,lcon,ucon)
 println("Butterfly method:")
 #résolution avec ALAS Butterfly
-xb,fb,orb = MPCCsolve.solve(ex3,r0,sig_r,s0,sig_s,t0,sig_t)
+@time xb,fb,orb = MPCCsolve.solve(ex3,r0,sig_r,s0,sig_s,t0,sig_t)
 
 if plot
  subplot(313)
