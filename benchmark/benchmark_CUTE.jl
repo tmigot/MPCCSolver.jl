61cc96cf951044127ae2bf78dacbe65b284d48cb
tmigot <tangi.migot@gmail.com>
2017-06-30 21:12:40 -0400

    Add files via upload

--- benchmark/benchmark_CUTE.jl
+++ benchmark/benchmark_CUTE.jl
@@ -3,6 +3,10 @@ include("../ALAS/include.jl")
 
 using MPCCmod
 using OutputRelaxationmod
+using Penalty
+using DDirection
+using LineSearch
+using AlgoSetmod
 
 using CUTEst
 using NLPModels
@@ -10,37 +14,50 @@ using NLPModels
 using BenchmarkProfiles
 
 problems_unconstrained = CUTEst.select(contype="unc")
-problems_linconstrained = CUTEst.select(contype="linear")
+problems_linconstrained = CUTEst.select(contype="linear",min_var=1, max_var=10)
 problems_boundconstrained = CUTEst.select(contype="bounds")
 problems_quadconstrained = CUTEst.select(contype="quadratic")
 problems_genconstrained = CUTEst.select(contype="general")
-problems=CUTEst.select()
-n=length(problems)
+problems=CUTEst.select(min_var=1, max_var=10)
+m=length(problems)
 
 #USER Choice :
 
-pb=problems_unconstrained
-n=5
+pb=problems
+n=length(pb)
+
+algo1=AlgoSetmod.AlgoSet(Penalty.Quadratic,DDirection.CGHZ,LineSearch.ArmijoWolfe)
+algo2=AlgoSetmod.AlgoSet(Penalty.Quadratic,DDirection.CGHZ,LineSearch.ArmijoWolfeHZ)
+algo3=AlgoSetmod.AlgoSet(Penalty.Quadratic,DDirection.CGHZ,LineSearch.Armijo)
 
 #....
 
 T=zeros(n,3)
 
-@printf("Start of a benchmark test from CUTEst: %i \n",n)
+@printf("Start of a benchmark test from CUTEst with %i problems.\n",n)
 
-for i=1:min(n,length(pb))
+for i=1:min(n,m)
  nlp = CUTEstModel(pb[i])
- @printf("CUTEst %i: %s ",i,nlp.meta.name)
+ @printf("CUTEst %i: %s \n",i,nlp.meta.name)
 
  #resolution avec ALAS
- exemple_nlp=MPCCmod.MPCC(nlp)
+ exemple_nlp=MPCCmod.MPCC(nlp,algo1)
  xb,fb,orb,nb_eval = MPCCsolve.solve(exemple_nlp)
  @printf("%s ",orb.solve_message)
  println(nb_eval)
+ T[i,1]=orb.solve_message=="Success"?sum(nb_eval):Inf
 
- T[i,1]=sum(nb_eval)
- T[i,2]=sum(nb_eval)+2*rand()-1
- T[i,3]=sum(nb_eval)+2*rand()-1
+ exemple_nlp=MPCCmod.MPCC(nlp,algo2)
+ xb,fb,orb,nb_eval = MPCCsolve.solve(exemple_nlp)
+ @printf("%s ",orb.solve_message)
+ println(nb_eval)
+ T[i,2]=orb.solve_message=="Success"?sum(nb_eval):Inf
+
+ exemple_nlp=MPCCmod.MPCC(nlp,algo3)
+ xb,fb,orb,nb_eval = MPCCsolve.solve(exemple_nlp)
+ @printf("%s ",orb.solve_message)
+ println(nb_eval)
+ T[i,3]=orb.solve_message=="Success"?sum(nb_eval):Inf
 
  finalize(nlp)
 end
